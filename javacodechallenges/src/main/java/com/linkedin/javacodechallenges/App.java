e0491e1bc8bf3e0417b5257cecaf1cd726549db6
Carson McKinstry <carsonm@spotify.com>
2024-01-15 16:16:15 +0100

    challenge 1_5 complete without tests

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -2,8 +2,11 @@ package com.linkedin.javacodechallenges;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        
+    public static void main( String[] args ) {
+        Person carson = new Person("Carson", "McKinstry", 30);
+        Person carina = new Person("Carina", "Reidick", 32);
+
+        carson.introduction();
+        carina.introduction();
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,46 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    if (age < this.age) {
+      System.out.printf("You can't make a person younger! New age \"%d\" not set", age);
+    } else {
+      this.age = age;
+    }
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public void setFirstName(String newName) {
+    this.firstName = newName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setLastName(String newName) {
+    this.lastName = newName;
+  }
+
+  public void introduction() {
+    System.out.printf("Hello my name is %s %s! I am %d years old.\n", this.firstName, this.lastName, this.age);
+  }
 }
